b1f6f65b678bab46b07a5618ced0c3c38708658c
南保 俊輔 <166090357+s2422051@users.noreply.github.com>
2025-08-30 18:17:22 +0900

    マイページそれっぽくできたよーん

--- templates/header.php
+++ templates/header.php
@@ -1,25 +1,38 @@
-<?php require_once __DIR__ . '/../helpers.php'; ?>
+<?php 
+require_once __DIR__ . '/../helpers.php'; 
+$is_logged_in = is_logged_in();
+if ($is_logged_in) {
+    // ログイン中のユーザー情報を取得
+    $current_user = get_user_by_id($_SESSION['user_id']);
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <title>AI Fashion Coordinator</title>
+    <title>ファッション共有サイト</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>/style.css">
 </head>
 <body>
     <header>
-        <h1><a href="<?= BASE_URL ?>/">AI Fashion Coordinator</a></h1>
-        <nav>
-            <a href="<?= BASE_URL ?>/">ホーム</a>
-            <?php if (is_logged_in()): ?>
-                <a href="<?= BASE_URL ?>/post.php">投稿する</a>
-                <a href="<?= BASE_URL ?>/closet.php">クローゼット</a>
-                <a href="<?= BASE_URL ?>/mypage.php">マイページ</a>
-                <a href="<?= BASE_URL ?>/logout.php">ログアウト</a>
-            <?php else: ?>
-                <a href="<?= BASE_URL ?>/login.php">ログイン</a>
-                <a href="<?= BASE_URL ?>/register.php">新規登録</a>
-            <?php endif; ?>
-        </nav>
+        <div class="header-container">
+            <h1><a href="<?= BASE_URL ?>/">ファッション共有サイト</a></h1>
+            <nav>
+                <a href="<?= BASE_URL ?>/">ホーム</a>
+                <?php if ($is_logged_in): ?>
+                    <a href="<?= BASE_URL ?>/post.php">投稿</a>
+                    <a href="<?= BASE_URL ?>/mypage.php">マイページ</a>
+                    <a href="<?= BASE_URL ?>/logout.php">ログアウト</a>
+                    
+                    <div class="nav-user-info">
+                        <?= htmlspecialchars($current_user['username']) ?> さん
+                    </div>
+                <?php else: ?>
+                    <a href="<?= BASE_URL ?>/login.php">ログイン</a>
+                    <a href="<?= BASE_URL ?>/register.php">新規登録</a>
+                <?php endif; ?>
+            </nav>
+        </div>
     </header>
-    <main>
+    <main class="container">
